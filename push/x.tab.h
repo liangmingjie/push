2108530109a819c6811bc1d23345504de0fe4aae
npe <npe@isam72.be.alcatel-lucent.com>
2009-11-03 15:48:47 +0100

    initial commit

---
+++ push/x.tab.h
@@ -0,0 +1,32 @@
+#define	FOR	57346
+#define	IN	57347
+#define	WHILE	57348
+#define	IF	57349
+#define	NOT	57350
+#define	TWIDDLE	57351
+#define	BANG	57352
+#define	SUBSHELL	57353
+#define	SWITCH	57354
+#define	FN	57355
+#define	WORD	57356
+#define	REDIR	57357
+#define	DUP	57358
+#define	PIPE	57359
+#define	FANIN	57360
+#define	FANOUT	57361
+#define	SUB	57362
+#define	SIMPLE	57363
+#define	ARGLIST	57364
+#define	WORDS	57365
+#define	BRACE	57366
+#define	PAREN	57367
+#define	PCMD	57368
+#define	PIPEFD	57369
+#define	ANDAND	57370
+#define	OROR	57371
+#define	COUNT	57372
+
+typedef union {
+	struct tree *tree;
+}	YYSTYPE;
+extern	YYSTYPE	yylval;
